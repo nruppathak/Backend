138cffb99f63c097c6b818befe6166daae0991fb
Nrup Pathak <nruppathak@gmail.com>
2026-02-11 10:32:43 +0530

    organise

---
+++ Assingnment/Assignment 1/organise.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p img pdf video txt mp3
+
+for file in *; do
+    [ -f "$file" ] || continue
+
+    case "$file" in
+        *.jpg|*.png)   mv "$file" img/ ;;
+        *.pdf)        mv "$file" pdf/ ;;
+        *.mp4|*.mkv)  mv "$file" video/ ;;
+        *.txt)        mv "$file" txt/ ;;
+        *.mp3)        mv "$file" mp3/ ;;
+    esac
+done
